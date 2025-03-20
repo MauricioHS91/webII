03c3a48256ff87209481513ccf66bda003aa2bc7
MauricioHS91 <145577830+MauricioHS91@users.noreply.github.com>
2025-03-20 00:15:52 +0000

    2025-03-19

---
+++ MauricioHansen_Lista01/ex1/ex1/src/main/java/aplicativo/Principal.java
@@ -0,0 +1,37 @@
+package aplicativo;
+
+import dominio.Pessoa;
+import dominio.Vendedor;
+import dominio.Cliente;
+import java.util.ArrayList;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        // Instancia o EntityManagerFactory com as configurações de persistência
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("aula-jpa"); 
+        EntityManager em = emf.createEntityManager(); // Instancia o EntityManager
+        
+        // Criando as instâncias de Vendedor e Cliente
+        Vendedor vendedor1 = new Vendedor("Maurício", 33, 1);
+        Cliente cliente1 = new Cliente("Rafael", 34, 1);
+        
+        // Iniciar transação com o banco de dados
+        em.getTransaction().begin();
+
+        // Persistir as instâncias de Vendedor e Cliente
+        em.persist(vendedor1);
+        em.persist(cliente1);
+        
+        em.getTransaction().commit();
+        
+        // Fechar o EntityManager e EntityManagerFactory
+        emf.close();
+        em.close();
+    }            
+}
